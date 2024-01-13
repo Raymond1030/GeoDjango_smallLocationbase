d7ad8ed64eed5fb26f3294ad32f09e3f7ffe3520
Raymond1030 <loho1030@gmail.com>
2024-01-14 00:54:35 +0800

    admin 管理员Web端可用于上传位置信息

--- MobileCloud/LocationAndroidCloud/admin.py
+++ MobileCloud/LocationAndroidCloud/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Location
 from django.contrib.gis.admin import OSMGeoAdmin
 
-admin.site.register(Location)
+
 
 
 class LocationAdmin(OSMGeoAdmin):
     list_display = ('name', 'geom')
+
+
+admin.site.register(Location, LocationAdmin)
